421ec76ee7b9a3cdccfe6c65a9d2f86d13bbd73f
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2019-05-18 15:20:56 +0200

    ntp time, rtc

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,26 @@
 #include <Arduino.h>
 
-#include <RecurringTask.h>
-
+//
 #include <Time.h>
 #include <TimeLib.h>
 
 #include <Timezone.h>
 
+//
+#include <WiFiUdp.h>
 #include <NTPClient.h>
 
+//
 #include <Wire.h>
 #include <RtcDS3231.h>
 
+//
 #include <WiFiConfig.h>
 #include <DNSServer.h>
+
+//
+#include <RecurringTask.h>
+#include <WiFiConfig.h>
 
 #include "LedMatrixDisplay.h"
 
@@ -33,6 +40,14 @@ typedef DisplayColumnCathode<TCanvas, 2> TDisplay;
 
 TDisplay display;
 
+//
+ConfigManager configManager;
+
+struct Config
+{
+
+} config;
+
 // Time zone
 TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 60 * 2};
 TimeChangeRule CET = {"CET", Last, Sun, Oct, 2, 60 * 1};
@@ -42,6 +57,9 @@ Timezone tzBerlin(CEST, CET);
 const char *ntpServer = "pool.ntp.org";
 const int ntpIntervalShort = 60;         // short interval while RTC clock are incorrect
 const int ntpIntervalLong = 60 * 60 * 6; // long interval while RTC clock are correct
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, ntpServer, ntpIntervalShort, ntpIntervalLong);
 
 // RTC clock DS3231
 RtcDS3231<TwoWire> Rtc(Wire);
@@ -82,6 +100,13 @@ void setup()
   display.drawText(7, 0, (char *)"  :  ");
   display.commit();
 
+  //
+  configManager.setAPName("Matrix Clock v3");
+  configManager.begin(config);
+
+  //
+  timeClient.begin();
+
   // Init RTC
   Rtc.Begin();
   Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeClock);
@@ -92,10 +117,52 @@ void setup()
   // attachInterrupt(digitalPinToInterrupt(pinInterrupt), onSquareWave, FALLING);
 }
 
+bool dots = false;
+
+void drawTime(int x, int y, time_t local, bool dots)
+{
+  int h = hour(local);
+  int m = minute(local);
+  int s = second(local);
+  // bool dots = rtcSquareCouter < 512;
+
+  // draw time
+  if (h >= 10)
+  {
+    display.drawChar(x, y, digit(h / 10));
+  }
+  x += 6;
+
+  display.drawChar(x, y, digit(h % 10));
+  x += 6;
+
+  // draw dots
+  if (dots)
+  {
+    display.drawChar(x, y, ':');
+  }
+  x += 4;
+
+  //
+  display.drawChar(x, y, digit(m / 10));
+  x += 6;
+  display.drawChar(x, y, digit(m % 10));
+  x += 6;
+}
+
+/**
+ * @brief
+ *
+ */
 void loop()
 {
-  bool dots = false;
+  //
+  configManager.loop();
 
+  //
+  timeClient.update();
+
+  //
   RecurringTask::interval(500, [&]() {
     dots = !dots;
   });
@@ -104,73 +171,44 @@ void loop()
     // Clear display
     display.clear();
 
-    // //
-    // if (!Rtc.IsDateTimeValid())
-    // {
-    //   if (WiFi.status() != WL_CONNECTED)
-    //   {
-    //     // not connected
-    //     display.drawText(7, 0, "  :  ");
-    //     Serial.println("Connecting to WiFi...");
-    //   }
-    //   else
-    //   {
-    //     // connected but time is incorrect
-    //     display.drawText(7, 0, "--:--");
-    //     Serial.println("RTC lost confidence in the DateTime!");
-    //   }
-
-    //   display.drawText();
-
-    //   NTP.getTime();
-    // }
-    // else
-    // {
-    //   // get RTC time
-    RtcDateTime utc = Rtc.GetDateTime();
-
-    // convert UTC time to local
-    time_t local = tzBerlin.toLocal(utc.Epoch32Time());
-
-    int h = hour(local);
-    int m = minute(local);
-    int s = second(local);
-    // bool dots = rtcSquareCouter < 512;
-
-    int x = 0;
-
-    // draw time
-    if (h >= 10)
-    {
-      display.drawChar(x, 0, digit(h / 10));
-    }
-    x += 6;
-
-    display.drawChar(x, 0, digit(h % 10));
-    x += 6;
-
-    // draw dots
-    if (dots)
-    {
-      display.drawChar(x, 0, ':');
-    }
-    x += 4;
-
     //
-    display.drawChar(x, 0, digit(m / 10));
-    x += 6;
-    display.drawChar(x, 0, digit(m % 10));
-    x += 6;
+    if (!Rtc.IsDateTimeValid())
+    {
+      if (WiFi.status() != WL_CONNECTED)
+      {
+        // not connected
+        display.drawText(7, 0, "  :  ");
+        Serial.println("Connecting to WiFi...");
+      }
+      else
+      {
+        // connected but time is incorrect
+        display.drawText(7, 0, "--:--");
+        Serial.println("RTC lost confidence in the DateTime!");
 
-    x += 4;
+        if (timeClient.forceUpdate())
+        {
+          time_t now = timeClient.getEpochTime();
 
-    //
-    display.drawChar(x, 0, digit(s / 10));
-    x += 6;
-    display.drawChar(x, 0, digit(s % 10));
-    x += 6;
+          Serial.print("Got NTP time: ");
+          Serial.println(timeClient.getFormattedTime());
 
-    // }
+          // update RTC time
+          RtcDateTime timeNow = RtcDateTime(year(now), month(now), day(now), hour(now), minute(now), second(now));
+          Rtc.SetDateTime(timeNow);
+        }
+      }
+    }
+    else
+    {
+      // get RTC time
+      RtcDateTime utc = Rtc.GetDateTime();
+
+      // convert UTC time to local
+      time_t local = tzBerlin.toLocal(utc.Epoch32Time());
+
+      drawTime(7, 0, local, dots);
+    }
 
     //
     display.commit();
